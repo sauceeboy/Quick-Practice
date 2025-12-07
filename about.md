74c7dcba629192f6839e351b0167df9096515275
Konner Hinds <37933@ghaps.org>
2025-12-06 22:18:08 -0500

    Initial commit

---
+++ about.md
@@ -0,0 +1,12 @@
+# Quick Practice
+
+A simple mod that adds a <cg>practice mode</c> button to the ouside of levels, there are 2 settings.
+
+## <cr>Auto Practice</c>
+- Removes the <cg>practice mode</c> button alltogether and automaticlly puts the player in <cg>practice mode</c> on enter.
+
+## <co>Layout</c>
+- Small Practice: Makes the <cg>practice mode</c> button small and the <cy>play</c> button normal size.
+- Same Size: Makes the <cg>practice mode</c> button and the <cy>play</c> button the same size (in my opinion the other option looks better).
+
+Thank you for dowloading my mod, updates will be soon to come!
---
+++ src/main.cpp
@@ -0,0 +1,81 @@
+#include <Geode/Geode.hpp>
+#include <Geode/modify/LevelInfoLayer.hpp>
+#include <Geode/modify/PlayLayer.hpp>
+
+using namespace geode::prelude;
+
+bool g_forcePractice = false;
+
+class $modify(MyLevelInfoLayer, LevelInfoLayer) {
+	public:
+
+	bool init(GJGameLevel* level, bool challenge) {
+		if (!LevelInfoLayer::init(level, challenge)) return false;
+
+		bool autoPrac = Mod::get()->getSettingValue<bool>("autopractice");
+		std::string layout = Mod::get()->getSettingValue<std::string>("layout");
+
+		auto menu = this->getChildByID("play-menu");
+
+		auto playButton = menu->getChildByID("play-button");
+		
+		auto practiceButton = CCMenuItemSpriteExtra::create(
+			CCSprite::createWithSpriteFrameName("GJ_practiceBtn_001.png"),
+			this,
+			menu_selector(MyLevelInfoLayer::onPracticeButton)
+		);
+
+		if (layout == "Small Practice") {
+		practiceButton->setPosition( {-55, 51} );
+		practiceButton->setID("practice-button"_spr);
+		practiceButton->setScale(0.75);
+		practiceButton->m_baseScale = 0.75;
+		playButton->setPositionX(8.00);
+	} else if (layout == "Same Size") {
+		practiceButton->setPosition( {-40, 51} );
+		practiceButton->setID("practice-button"_spr);
+		practiceButton->setScale(1.300);
+		practiceButton->m_baseScale = 1.300;
+		playButton->setPositionX(39.00);
+	}
+
+		if (autoPrac) {
+		g_forcePractice = true;
+		practiceButton->m_bEnabled = false;
+		practiceButton->m_bVisible = false;
+		playButton->setPositionX(0.00);
+	} else {
+		g_forcePractice = false;
+		practiceButton->setID("practice-button"_spr);
+		practiceButton->m_bEnabled = true;
+		practiceButton->m_bVisible = true;
+	}
+
+		menu->addChild(practiceButton);
+		return true;
+	}
+
+	void onPracticeButton(CCObject*) {
+		g_forcePractice = true;
+
+		auto menu = this->getChildByID("play-menu");
+		auto playButton = menu->getChildByType<CCMenuItemSpriteExtra>(0);
+
+		playButton->activate();
+	};
+};
+
+class $modify(MyPlayLayer, PlayLayer) {
+	bool init(GJGameLevel* level, bool useReplay, bool dontCreateObjects) {
+		if (!PlayLayer::init(level, useReplay, dontCreateObjects)) return false;
+		
+		if (g_forcePractice) {
+			this->togglePracticeMode(true);
+		} else {
+			this->togglePracticeMode(false);
+		}
+
+		g_forcePractice = false;
+		return true;
+	}
+};
